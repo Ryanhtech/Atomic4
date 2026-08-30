fff0653d9d25d9e2300de376f5d44763dec674cc
Ryanhtech Labs <hello.ryanhtech.labs@gmail.com>
2026-08-30 17:50:43 +0200

    Add AtomicGame implementation file

---
+++ Engine/Source/Game/Game.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Game.cpp
+ * @brief Game implementation
+ * 
+ * @copyright Copyright (c) 2026 Ryanhtech Labs.
+ * 
+ */
+
+#include <AtomicGame.hpp>
+
+Atomic::Game::Game()
+{
+
+}
+
+Atomic::Game::~Game()
+{
+    
+}
---
+++ Engine/Source/Math/RGB.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file RGB.cpp
+ * @brief RGB implementation
+ * 
+ * @copyright Copyright (c) 2026 Ryanhtech Labs.
+ * 
+ */
+
+#include <AtomicMath.hpp>
+
+Atomic::RGB::RGB(uint8_t r, uint8_t g, uint8_t b)
+{
+    r = r;
+    g = g;
+    b = b;
+}
+
+Atomic::RGB::~RGB()
+{
+
+}
