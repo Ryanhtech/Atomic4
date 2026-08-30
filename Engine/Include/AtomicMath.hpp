bd555d380aa8b2ff96a7e23b2cca920a32e1544a
Ryanhtech Labs <hello.ryanhtech.labs@gmail.com>
2026-08-30 17:43:35 +0200

    Add Atomic::RGB class declaration

--- Engine/Include/AtomicMath.hpp
+++ Engine/Include/AtomicMath.hpp
@@ -1,4 +1,6 @@
 
+#include <cinttypes>
+
 #ifndef __ATOMIC_MATH_HPP__
 #define __ATOMIC_MATH_HPP__
 
@@ -29,6 +31,31 @@ namespace Atomic
         float x;
         float y;
         float z;
+    };
+
+    class RGB
+    {
+    public:
+        /**
+         * @brief Constructs a new RGB instance. All values are stored as
+         * 8-bit unsigned integers.
+         * 
+         * @param r Red value.
+         * @param g Green value.
+         * @param b Blue value.
+         */
+        RGB(uint8_t r, uint8_t g, uint8_t b);
+
+        /**
+         * @brief Destroys the RGB instance.
+         * 
+         */
+        ~RGB();
+
+        // -- Values --
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
     };
 }
 
